fd89277d539c283de3597a78e3a01e29734f2ad2
Epicgamergrub17 <92122930+Epicgamergrub17@users.noreply.github.com>
2021-10-08 12:22:57 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Well Well Well, If it isn'y the actions of my consequences.");
+  }
+}
+/*this is a comment*/
+
+/*Java Variables Typo
+You have 5 variables in Java
+Strings : Text - "Hello"
+Integer : Int - Always WHOLE numbers
+Boolean : true or false
+Floats : Decimals - 19.999
+Char : Single characters - "a" "b" "c"
+How many charactors does pneumonoultramicroscopicsilicovolcanoconiosis have?
+
+In math if x=4 then 4=x
+In programing if x=4 then 4 is assigned to x
+
+
+
+x = "patty"
+b = "tomato"
+c = "cheese"
+d = "bread"
+e = "lettuce"
+f = "ketchup"
+g = "mayo"
+
+So the template is : tyoe variable = value;
+*/
